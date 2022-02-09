8e2c77b14ac49ce590f4cbaf76cfe6283af979b5
Michael Lyon <lyoncodes@gmail.com>
2022-02-09 00:34:29 -0800

    adds the buildMenu() function to Menu class that creates HTML for each item in the menu, creates menu array of MenuItem objects, adds methods to MenuItem for getting instance properties and option menu, adds some basic menu css

--- classes/Menu.php
+++ classes/Menu.php
@@ -1,2 +1,36 @@
 <?php
 // Menu.php
+class Menu {
+  public $title;
+  public $menuData;
+
+  public function __construct($menuData, $title) {
+    $this->title = $title;
+    $this->menuData = $menuData;
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+
+  public function buildMenu() {
+    $str = '<h1>'.$this->getTitle().'</h1>';
+
+    foreach($this->menuData as $menuItem) {
+      $str .= 
+      '<div class="item-container">
+        <h2>'.$menuItem->getName().'</h2>
+        <p>'.$menuItem->getDesc().'</p>
+        <p>'.$menuItem->getPrice().'</p>
+        <select>'.$menuItem->getOption($menuItem->getMax(), $menuItem->getName()).'</select>
+      </div>';
+    }
+    echo $str;
+  }
+
+  public function calculateTotal($cart){
+    // kata:
+    // sum the prices of the objects stored in the $cart array
+    // output the total
+  }
+}
