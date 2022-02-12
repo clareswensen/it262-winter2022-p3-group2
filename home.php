e1234b50ba665b8fc7695f01a3ccef09efadf5f4
jmgarza94 <josele.garza94@outlook.com>
2022-02-11 20:14:11 -0800

    started building and styling home.php with bootstrap

--- home.php
+++ home.php
@@ -23,13 +23,42 @@ $menu->calculateTotal();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Latest compiled and minified CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
   <link href="style.css" rel="stylesheet">
   <title>IT262 P3: Food Truck</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header ms-5">
+      <a class="navbar-brand" href="#">Con Amigos Taco Truck</a>
+    </div>
+    <ul class="nav navbar-nav ms-auto">
+      <li class="nav-item me-5"><a href="#" class="nav-link active">Home</a></li>
+      <li class="nav-item me-5"><a href="#" class="nav-link">Menu</a></li>
+      <li class="nav-item me-5"><a href="#" class="nav-link">Locations</a></li>
+      <li class="nav-item me-5"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="row">
+  <div class="col-sm-8">
   <form action="" method="POST">
     <?php $menu->buildMenu() ;?>
     <input type="submit"> Add To Cart
   </form>
+  </div>
+  <div class="col-sm-4">
+    <div class="container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure labore eligendi, nam quibusdam exercitationem quaerat sequi deleniti soluta tenetur blanditiis obcaecati cum! Modi eligendi, earum rerum similique nesciunt repellat temporibus!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure labore eligendi, nam quibusdam exercitationem quaerat sequi deleniti soluta tenetur blanditiis obcaecati cum! Modi eligendi, earum rerum similique nesciunt repellat temporibus!</p>
+    </div>
+  </div>
+</div>
 </body>
 </html>
